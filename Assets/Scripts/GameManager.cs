5c6a68470b2303d1694dd88738ad38f03f7e6714
ISLEM-PM <m.islem@mt-uae.com>
2025-07-17 02:47:11 +0400

    Main menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+namespace MatchingGame
+{
+    public class GameManager : MonoBehaviour
+    {
+        private static GameManager instance;
+
+        [SerializeField] private DifficultyHandler difficultyHandler;
+
+        public static GameManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = FindObjectOfType<GameManager>();
+
+                    if (instance == null)
+                    {
+                        GameObject singletonObject = new GameObject("GameManager");
+                        instance = singletonObject.AddComponent<GameManager>();
+                    }
+                }
+                return instance;
+            }
+        }
+
+        private void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else if (instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        public void LaunchGame()
+        {
+            SceneManager.LoadSceneAsync("Matching");
+        }
+
+    }
+
+}
