94f3fcfdf3d0dfa85039dd7d249c55b2b560fcd9
ISLEM-PM <m.islem@mt-uae.com>
2025-07-20 00:34:25 +0400

    Added Game Audio

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,130 @@
+using UnityEngine;
+
+namespace MatchingGame
+{
+    public class AudioManager : MonoBehaviour
+    {
+        public static AudioManager instance;
+
+        [Header("Sources")]
+        [SerializeField] AudioSource musicSource;
+        [SerializeField] AudioSource sfxSource;
+
+        [Header("Clips")]
+        [SerializeField] AudioClip startMusic;
+        [SerializeField] AudioClip flipClip;
+        [SerializeField] AudioClip matchClip;
+        [SerializeField] AudioClip mismatchClip;
+
+        [Header("Volumes (0..1)")]
+        [Range(0, 1f)] public float masterVolume = 1f;
+        [Range(0, 1f)] public float musicVolume = 1f;
+        [Range(0, 1f)] public float sfxVolume = 1f;
+
+        [Header("SFX Pitch")]
+        [SerializeField] bool randomizePitch = true;
+        [SerializeField] float pitchVar = 0.1f; // ± variation
+
+        public static AudioManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = FindObjectOfType<AudioManager>();
+
+                    if (instance == null)
+                    {
+                        GameObject singletonObject = new GameObject("AudioManager");
+                        instance = singletonObject.AddComponent<AudioManager>();
+                    }
+                }
+                return instance;
+            }
+        }
+        void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else if (instance != this)
+            {
+                Destroy(gameObject);
+            }
+            DontDestroyOnLoad(gameObject);
+            InitSources();
+            ApplyVolumes();
+
+            if (startMusic) PlayMusic(startMusic);
+        }
+
+        void InitSources()
+        {
+            if (!musicSource)
+            {
+                musicSource = gameObject.AddComponent<AudioSource>();
+                musicSource.loop = true;
+                musicSource.playOnAwake = false;
+            }
+            if (!sfxSource)
+            {
+                sfxSource = gameObject.AddComponent<AudioSource>();
+                sfxSource.loop = false;
+                sfxSource.playOnAwake = false;
+            }
+        }
+
+        // -------- MUSIC (no fading) --------
+        public void PlayMusic(AudioClip clip)
+        {
+            if (!clip) return;
+            musicSource.clip = clip;
+            musicSource.volume = masterVolume * musicVolume;
+            musicSource.loop = true;
+            musicSource.Play();
+        }
+
+        public void StopMusic()
+        {
+            musicSource.Stop();
+        }
+
+        public void ChangeMusic(AudioClip newClip) => PlayMusic(newClip);
+
+        // -------- SFX --------
+        public void PlaySfx(AudioClip clip)
+        {
+            if (!clip) return;
+            sfxSource.pitch = randomizePitch ? Random.Range(1f - pitchVar, 1f + pitchVar) : 1f;
+            sfxSource.PlayOneShot(clip, masterVolume * sfxVolume);
+        }
+
+        public void PlayFlip() => PlaySfx(flipClip);
+        public void PlayMatch() => PlaySfx(matchClip);
+        public void PlayMismatch() => PlaySfx(mismatchClip);
+
+        // -------- VOLUME CONTROL --------
+        public void SetVolumes(float master, float music, float sfx)
+        {
+            masterVolume = Mathf.Clamp01(master);
+            musicVolume = Mathf.Clamp01(music);
+            sfxVolume = Mathf.Clamp01(sfx);
+            ApplyVolumes();
+        }
+
+        void ApplyVolumes()
+        {
+            if (musicSource.isPlaying)
+                musicSource.volume = masterVolume * musicVolume;
+            // SFX volume applied at PlayOneShot call
+        }
+
+        public void MuteAll(bool mute)
+        {
+            musicSource.mute = mute;
+            sfxSource.mute = mute;
+        }
+    }
+}
